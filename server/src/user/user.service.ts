6aa71000fe0dff93b16547e3dc8b243d91db5af7
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-08 00:03:53 +0200

    <server> feat(user): injected user model to user service

---
+++ server/src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { User, UserDocument } from './user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+
+  create(createUserInput: CreateUserInput) {
+    return `This action adds a new user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserInput: UpdateUserInput) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
