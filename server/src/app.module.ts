04ec265ac434e5fa5917e1861acfcb17f5584bc8
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-07 23:52:00 +0200

    <server> feat(app): imported graphql module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 
 @Module({
   imports: [
     UserModule,
-    MongooseModule.forRoot(process.env.MONGO_URI)]
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    })
+  ]
 })
-export class AppModule {}
+export class AppModule { }
