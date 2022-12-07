d2cf57e2ce16abfac5312b9be28800a3713ac66b
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-08 00:14:14 +0200

    <server> feat(auth): auth service provider

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Module({
-    imports: [UserModule]
+    imports: [UserModule],
+    providers: [AuthService],
 })
 export class UserModule {}
