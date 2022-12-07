23568f79ae8020e207bd847aa6d7f7ff3e14c30d
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-07 22:07:29 +0200

    <server> feat(app): mongoose connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule]
+  imports: [
+    UserModule,
+    MongooseModule.forRoot('mongodb://localhost/library')]
 })
 export class AppModule {}
