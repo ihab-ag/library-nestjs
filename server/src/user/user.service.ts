a749bd9a39639c857f55ff3a516469995bf26bfa
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-08 17:25:51 +0200

    <server> feat(user): implemented create user

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateUserInput } from './dto/create-user.input';
+import { UserCredentialsInput } from './dto/user-credentials.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User, UserDocument } from './user.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) {}
+  constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) { }
 
-  create(createUserInput: CreateUserInput): Promise<User> {
-    const user = new this.UserModel(createUserInput);
+  async create(userCredentialsInput: UserCredentialsInput): Promise<User> {
+
+    const hashed_password = await bcrypt.hash(userCredentialsInput.password, 10);
+
+    userCredentialsInput.password = hashed_password;
+
+    const user = new this.UserModel(userCredentialsInput);
+
     return user.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.UserModel.find().exec();
   }
 
   async findUsername(username: string): Promise<User> {
