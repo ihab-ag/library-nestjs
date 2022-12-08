07765fb9dc24b1d7a1910508f22780ec95b6a1fe
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-08 22:14:59 +0200

    <server> feat(auth): imported passport module and local strategy

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import {PassportModule} from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-    imports: [UserModule],
-    providers: [AuthService, AuthResolver],
+imports: [UserModule, PassportModule],
+    providers: [AuthService, AuthResolver, LocalStrategy],
 })
 export class AuthModule {}
