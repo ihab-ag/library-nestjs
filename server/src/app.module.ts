63bd91e2527beed6ccedb68ad026b190c7eaec16
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-08 14:17:19 +0200

    <server> feat(app): imported configuration module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,16 +4,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { join } from 'path';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    UserModule,
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    })
-  ]
+    }),
+    UserModule,
+  ],
 })
 export class AppModule { }
