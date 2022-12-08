f62a2c7f3abda59ffd3999bf4ff5b75dc48e08e0
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-08 22:15:22 +0200

    <server> feat(auth): login mutation

--- server/src/auth/auth.resolver.ts
+++ server/src/auth/auth.resolver.ts
@@ -1,7 +1,10 @@
 import { AuthService } from "./auth.service";
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Resolver, Context } from "@nestjs/graphql";
 import { UserCredentialsInput } from "src/user/dto/user-credentials.input";
 import { User } from "src/user/entities/user.entity";
+import { LoginResponse } from "./dto/login-response";
+import { LocalAuthGuard } from "./guards/local.guard";
+import { UseGuards } from "@nestjs/common";
 
 @Resolver()
 export class AuthResolver {
@@ -10,5 +13,11 @@ export class AuthResolver {
     @Mutation(() => User)
     signup(@Args('userCredentialsInput') userCredentialsInput: UserCredentialsInput) {
         return this.authService.signup(userCredentialsInput);
+    }
+
+    @Mutation(() => LoginResponse)
+    @UseGuards(LocalAuthGuard)
+    login(@Args('loginInput') loginInput: UserCredentialsInput, @Context() ctx) {
+        return this.authService.login(ctx.user._doc);
     }
 }
