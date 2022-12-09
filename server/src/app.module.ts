797aa1243cbe48d8b4b8c1f8c79e7bed873afb2c
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-09 13:50:52 +0200

    <server> feat(auth): register jwt

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
-import {PassportModule} from '@nestjs/passport';
+import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-imports: [UserModule, PassportModule],
+    imports: [
+        ConfigModule.forRoot(),
+        UserModule,
+        PassportModule,
+        JwtModule.register({
+            signOptions: { expiresIn: '24h' },
+            secret: process.env.JWT_SECRET
+        })],
     providers: [AuthService, AuthResolver, LocalStrategy],
 })
-export class AuthModule {}
+export class AuthModule { }
