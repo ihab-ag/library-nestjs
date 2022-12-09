4cc36848bd5584a8df0c25e28daa28c76a769d99
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-09 13:49:41 +0200

    <server> feat(auth): sign jwt access tokenon login

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,30 +1,36 @@
 import { Injectable } from "@nestjs/common";
 import { UserCredentialsInput } from "src/user/dto/user-credentials.input";
-import { User } from "src/user/user.schema";
 import { UserService } from "src/user/user.service";
 import * as bcrypt from 'bcrypt';
+import { LoginResponse } from "./dto/login-response";
+import { User } from "src/user/entities/user.entity";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) { }
+    constructor(private readonly userService: UserService,
+        private readonly jwtService: JwtService) { }
 
     async validateUser(username: string, pass: string): Promise<any> {
         const user = await this.userService.findUsername(username);
-        if (user && bcrypt.compare(pass, user.password)) {
+        if (user && await bcrypt.compare(pass, user.password)) {
             const { password, ...result } = user;
             return result;
         }
         return null;
     }
 
-    async signup(userCredentialsInput: UserCredentialsInput): Promise<User> {
+    async signup(userCredentialsInput: UserCredentialsInput): Promise<any> {
         return this.userService.create(userCredentialsInput);
     }
 
-    async login(user: User) {
+    async login(user: User): Promise<LoginResponse> {
 
         return {
-            access_token: 'jwt-token',
+            accessToken: this.jwtService.sign({
+                username: user.username,
+                sub: user._id
+            }),
             user
         };
     }
