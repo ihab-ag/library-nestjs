6019897705f09717b75d408c67daeeafaa9b972a
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-10 15:43:28 +0200

    <server> feat(user): added roles field

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Role } from './role.enum';
 
 @ObjectType()
 export class User {
@@ -8,4 +9,6 @@ export class User {
   @Field()
   username: string;
 
+  @Field(() => [String], { nullable: true })
+  roles?: Role[];
 }
