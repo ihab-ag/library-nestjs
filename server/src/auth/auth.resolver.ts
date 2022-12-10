3564abf45d396c8c3c823e905ab4ec8daccc9594
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-10 15:57:27 +0200

    <server> feat(auth): added roles guard on signup

--- server/src/auth/auth.resolver.ts
+++ server/src/auth/auth.resolver.ts
@@ -5,11 +5,17 @@ import { User } from "src/user/entities/user.entity";
 import { LoginResponse } from "./dto/login-response";
 import { LocalAuthGuard } from "./guards/local.guard";
 import { UseGuards } from "@nestjs/common";
+import { Roles } from "./decorators/roles.decorator";
+import { Role } from "src/user/entities/role.enum";
+import { JwtGuard } from "./guards/jwt.guard";
+import { RolesGuard } from "./guards/roles.guard";
 
 @Resolver()
 export class AuthResolver {
     constructor(private readonly authService: AuthService) { }
 
+    @Roles(Role.Admin)
+    @UseGuards(JwtGuard, RolesGuard)
     @Mutation(() => User)
     signup(@Args('userCredentialsInput') userCredentialsInput: UserCredentialsInput) {
         return this.authService.signup(userCredentialsInput);
