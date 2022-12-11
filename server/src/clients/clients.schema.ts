1c94113a962dafbd3afb9ec07ef79c9488be87b3
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-11 13:44:36 +0200

    <server> feat(clients): client mongoose schema

---
+++ server/src/clients/clients.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ClientDocument = HydratedDocument<Client>;
+
+@Schema()
+export class Client {
+    
+    @Prop({
+        required: true,
+    })
+    name: string;
+
+    @Prop({
+        required: true,
+    })
+    email: string
+
+}
+
+export const ClientSchema = SchemaFactory.createForClass(Client);
