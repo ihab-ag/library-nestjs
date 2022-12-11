7bc47f943e3977bf24c46b95ca034876cd6ba3dd
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-11 18:57:56 +0200

    <server> feat(books): added client field to resolver

--- server/src/books/books.resolver.ts
+++ server/src/books/books.resolver.ts
@@ -1,12 +1,16 @@
-import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID, ResolveField, Parent } from '@nestjs/graphql';
 import { BooksService } from './books.service';
 import { Book } from './entities/book.entity';
 import { CreateBookInput } from './dto/create-book.input';
 import { UpdateBookInput } from './dto/update-book.input';
+import { ClientsService } from 'src/clients/clients.service';
 
 @Resolver(() => Book)
 export class BooksResolver {
-  constructor(private readonly booksService: BooksService) {}
+  constructor(
+    private readonly booksService: BooksService,
+    private readonly clientsService: ClientsService
+    ) {}
 
   @Mutation(() => Book)
   createBook(@Args('createBookInput') createBookInput: CreateBookInput) {
@@ -28,8 +32,9 @@ export class BooksResolver {
     return this.booksService.update(updateBookInput.id, updateBookInput);
   }
 
-  // @Mutation(() => Book)
-  // removeBook(@Args('id', { type: () => ID }) id: string) {
-  //   return this.booksService.remove(id);
-  // }
+  @ResolveField()
+  client(@Parent() book: Book) {
+    const { client_id } = book;
+    return this.clientsService.findOne(client_id);
+  }
 }
