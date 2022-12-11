7a6c40fc63b884459d67213aaaf1a915d7c5e4a1
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-11 19:11:41 +0200

    <server> feat(clients): imported books module

--- server/src/clients/clients.module.ts
+++ server/src/clients/clients.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsResolver } from './clients.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Client, ClientSchema } from './clients.schema';
+import { BooksModule } from 'src/books/books.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Client.name , schema: ClientSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]),
+    forwardRef(() => BooksModule)
+  ],
   providers: [ClientsResolver, ClientsService],
   exports: [ClientsService]
 })
-export class ClientsModule {}
+export class ClientsModule { }
