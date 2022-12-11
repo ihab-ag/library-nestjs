8d01edbc2afa7cee7ed123a2c5cacf106438e89b
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-12-11 14:56:22 +0200

    <seerver> feat(books): imported books mongoose module to books module

---
+++ server/src/books/books.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { BooksResolver } from './books.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Book, BookSchema } from './books.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])],
+  providers: [BooksResolver, BooksService]
+})
+export class BooksModule {}
